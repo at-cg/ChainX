f70b9fd552a6527f395b1d951d1d46f6c75da469
Chirag Jain <cjain7@gatech.edu>
2020-12-20 18:15:42 +0530

    fix infix logic

--- src/arED.cpp
+++ src/arED.cpp
@@ -83,6 +83,7 @@ int redit_strong_prec(const std::vector<std::tuple<int, int, int>> &anchors)
 
 /**
  * @brief   compute anchor-restricted edit distance using strong precedence criteria
+ * 			optimized to run faster using engineering trick(s)
  **/
 int redit_strong_prec_optimized(const std::vector<std::tuple<int, int, int>> &anchors)
 {
@@ -153,7 +154,8 @@ int redit_strong_prec_optimized(const std::vector<std::tuple<int, int, int>> &an
 }
 
 /**
- * @brief   compute anchor-restricted edit distance using strong precedence criteria
+ * @brief   compute anchor-restricted (semi-global) edit distance using strong precedence criteria
+ * 			optimized to run faster using engineering trick(s)
  **/
 int redit_strong_prec_infix_optimized(const std::vector<std::tuple<int, int, int>> &anchors)
 {
@@ -183,6 +185,17 @@ int redit_strong_prec_infix_optimized(const std::vector<std::tuple<int, int, int
 			while (j_a - std::get<0>(anchors[inner_loop_start]) - 1 > bound_redit)
 				inner_loop_start++;
 
+			{
+				//always consider the first dummy anchor 
+				//connection to first dummy anchor is done with modified cost to allow free gaps
+				int i_d = std::get<1>(anchors[0]) + std::get<2>(anchors[0]) - 1;
+				int qry_gap = j_c - i_d - 1;
+				find_min_cost = std::min(find_min_cost, costs[0] + qry_gap);
+			}
+
+			//process all anchors in array for the final last dummy anchor
+			if (j == n-1) inner_loop_start=0;
+
 			for(int i=j-1; i>=inner_loop_start; i--)
 			{
 				int i_a = std::get<0>(anchors[i]);
@@ -195,7 +208,7 @@ int redit_strong_prec_infix_optimized(const std::vector<std::tuple<int, int, int
 					int gap1 = std::max(0, j_a - i_b - 1);
 					int gap2 = std::max(0, j_c - i_d - 1);
 
-					if (j==1 || j == n-1) gap1=0; //free terminal gap
+					if (j == n-1) gap1=0; //modified cost for the last dummy anchor to allow free gaps
 					int g = std::max(gap1,gap2);
 
 					int overlap1 = std::max(0, i_b - j_a + 1);
@@ -205,6 +218,7 @@ int redit_strong_prec_infix_optimized(const std::vector<std::tuple<int, int, int
 					find_min_cost = std::min(find_min_cost, costs[i] + g + o);
 				}
 			}
+
 			//save optimal cost at offset j
 			costs[j] = find_min_cost;
 		}
@@ -375,9 +389,60 @@ int redit_dp(const std::vector<std::tuple<int, int, int>> &anchors)
 		else
 			dp_matrix[i][j] = std::min({dp_matrix[i - 1][j - 1] + 1, dp_matrix[i - 1][j] + 1, dp_matrix[i][j - 1] + 1});
 	}
-  }
+	}
 
 	return dp_matrix[len_ref][len_qry];
+}
+
+/**
+ * @brief   compute anchor-restricted (semi-global) edit distance using standard edit-distance like dynamic programming 
+ **/
+int redit_dp_infix(const std::vector<std::tuple<int, int, int>> &anchors)
+{
+	int n = anchors.size();
+
+	//get sequence lengths from end dummy anchor
+	//assuming anchors are already sorted
+	int len_ref = std::get<0>(anchors[n-1]);
+	int len_qry = std::get<1>(anchors[n-1]);
+
+	//initialize a boolean matrix (len_ref+1 x len_qry+1)
+	//we will offset by 1 to be consistent with DP matrix
+	std::vector<std::vector<bool> > matchAllowed(len_ref+1);
+	for(int i=0; i<len_ref+1; i++) matchAllowed[i] = std::vector<bool>(len_qry+1, false);
+	for(int i = 0; i<n-1; i++) //use all (except end dummy) anchors
+	{
+	  int e_a = std::get<0>(anchors[i]);
+	  int e_c = std::get<1>(anchors[i]);
+	  int e_len = std::get<2>(anchors[i]);
+
+	  for(int j=0; j<e_len; j++)
+	  {
+		matchAllowed[e_a+1  + j][e_c+1  + j] = true;
+	  }
+	}
+
+
+	//initialize dp_matrix (len_ref+1 x len_qry+1) 
+	std::vector<std::vector<int> > dp_matrix(len_ref+1);
+	for(int i=0; i<=len_ref; i++) dp_matrix[i] = std::vector<int>(len_qry+1);
+	for(int i=0; i<=len_ref; i++) dp_matrix[i][0] = 0; //changed from i to 0 for free gaps
+	for(int j=0; j<=len_qry; j++) dp_matrix[0][j] = j;
+
+	for(int i=1; i<=len_ref; i++)
+	{
+	  for(int j=1; j<=len_qry; j++)
+	  {
+		if (matchAllowed[i][j])
+			dp_matrix[i][j] = std::min({dp_matrix[i - 1][j - 1], dp_matrix[i - 1][j] + 1, dp_matrix[i][j - 1] + 1});
+		else
+			dp_matrix[i][j] = std::min({dp_matrix[i - 1][j - 1] + 1, dp_matrix[i - 1][j] + 1, dp_matrix[i][j - 1] + 1});
+	  }
+	}
+
+	int final_distance = std::numeric_limits<int>::max();
+	for(int i=0; i<=len_ref; i++) final_distance = std::min (final_distance, dp_matrix[i][len_qry]);
+	return final_distance;
 }
 
 int main(int argc, char **argv) 
@@ -408,9 +473,13 @@ int main(int argc, char **argv)
 	{
 	  std::vector<std::tuple<int, int, int>> fwd_matches;
 	  mummer::mummer::sparseSA sa = mummer::mummer::sparseSA::create_auto(seq_ref.c_str(), seq_ref.length(), k, true);
+
+	  std::cerr << "Reference index computed" << "\n";
+
 	  auto append_matches = [&](const mummer::mummer::match_t& m) { fwd_matches.emplace_back(m.ref, m.query, m.len); }; //0-based coordinates
 	  sa.findMEM_each(seq_qry.c_str(), seq_qry.length(), k, false, append_matches);
 
+	  std::cerr << "Anchors have been identified" << "\n";
 
 	  //place dummy MEMs and then sort
 	  fwd_matches.emplace_back(-1,-1,1);
@@ -440,6 +509,8 @@ int main(int argc, char **argv)
 		  std::cout << redit_strong_prec_optimized(fwd_matches) << "\n";
 	  else if (method == 6)
 		  std::cout << redit_strong_prec_infix_optimized(fwd_matches) << "\n";
+	  else if (method == 7)
+		  std::cout << redit_dp_infix(fwd_matches) << "\n";
   }
 
 	return 0;
